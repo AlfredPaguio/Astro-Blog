187a27620545ce616fd2406013094d8551aa8b53
AlfredPaguio <alfredpaguio36@gmail.com>
2025-12-15 00:41:12 +0800

    6-islands and optional content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-      site: "https://blog-alfredpaguio.netlify.app/"
+  site: "https://blog-alfredpaguio.netlify.app/",
+  integrations: [preact()]
 });
